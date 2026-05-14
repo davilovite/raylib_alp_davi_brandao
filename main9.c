94c0f2412ca21e44487670e1d88128b478600bdc
davilovite <davibcavallari@gmail.com>
2026-05-14 18:21:31 -0300

    envio da atividade 4
    professor revise a atividade 4

---
+++ main9.c
@@ -0,0 +1,5 @@
+Respostas:
+1) O SetTargetFPS(60) foi o mais suave.
+2) O SetTargetFPS(10) foi o que ficou mais lento.
+3) O movimento continua o mesmo, o que muda é a velocidade dele.
+4) Sem o SetTargetFPS() o quadrado apenas executa o movimento em sua velocidade padrão predefinida, fica igual a atividade 3 mas sem loop.
