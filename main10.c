fc1f7506b8356a40189e75620d754773bd8766de
davilovite <davibcavallari@gmail.com>
2026-05-14 18:31:30 -0300

    envio do exercicio pratico de fps
    professor revise o exercicio pratico de fps

---
+++ main10.c
@@ -0,0 +1,29 @@
+#include "raylib.h"
+
+int main()
+{
+    int posX = 0;
+
+    InitWindow(800, 450, "basic window");
+    
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        posX++;
+
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawRectangle(posX, 100, 100, 100, BLUE);
+        
+        DrawFPS (10, 10);
+        
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
