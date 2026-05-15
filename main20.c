1112938c29a007a1de0519ae8440f8e8aa31e063
davilovite <davibcavallari@gmail.com>
2026-05-15 19:34:17 -0300

    envio do desafio extra da atividade 7
    professor revise o desafio extra da atividade 7

---
+++ main20.c
@@ -0,0 +1,74 @@
+#include "raylib.h"
+
+int main()
+{
+    float posX = -100; // posição inicial (fora da tela)
+
+    Texture2D feliz;
+
+    Sound tiro; // variável de som
+    Sound explosao;
+    Sound moeda;
+    
+    Music fundo;
+
+    InitWindow(800, 450, "basic window");
+
+    InitAudioDevice(); // inicia o sistema de áudio
+
+    feliz = LoadTexture("PROJETO_DAVI/feliz.png");
+
+    tiro = LoadSound("PROJETO_DAVI/tiro.MP3"); // carrega o som
+    explosao = LoadSound("PROJETO_DAVI/explosao.MP3");
+    moeda = LoadSound("PROJETO_DAVI/moeda.MP3");
+    
+    fundo = LoadMusicStream("PROJETO_DAVI/fundo.MP3");
+    
+    PlayMusicStream (fundo);
+
+    while (!WindowShouldClose())
+    {
+        posX += GetFrameTime() * 300;
+        
+        UpdateMusicStream (fundo);
+
+        if (posX > 800)
+        {
+            posX = -100;
+        }
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        {
+            PlaySound (tiro);
+            PlaySound (explosao);
+            PlaySound (moeda);
+        }
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            PlaySound (tiro);
+            PlaySound (explosao);
+            PlaySound (moeda);
+        }
+
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawTexture(feliz, posX, 10, WHITE);
+
+        EndDrawing();
+    }
+    
+    UnloadTexture(feliz);
+
+    UnloadSound(tiro);
+    UnloadSound(explosao);
+    UnloadSound(moeda);
+    
+    UnloadMusicStream(fundo);
+
+    CloseAudioDevice();
+
+    CloseWindow();
+
+    return 0;
+}
